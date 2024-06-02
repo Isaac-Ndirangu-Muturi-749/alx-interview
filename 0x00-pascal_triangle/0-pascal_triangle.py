64aed955fa26014484696c2600b3e1f3a4ac9dde
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-06-02 17:39:26 +0300

    Update 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
+
+
+def generate_pascals_triangle(num_rows):
+    """
+    Generate Pascal's triangle up to n rows.
+    """
+    triangle = []
+
+    if num_rows <= 0:
+        return triangle
+
+    for row_num in range(num_rows):
+        # Start with a row of "1"s
+        row = [1] * (row_num + 1)
+
+        # Compute the values inside the row
+        for j in range(1, row_num):
+            row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+        # Add the row to the triangle
+        triangle.append(row)
+
+    return triangle
