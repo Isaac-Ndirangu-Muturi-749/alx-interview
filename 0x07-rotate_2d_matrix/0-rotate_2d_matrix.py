34bc6e13c17c8797425aef7421e8f974648a5869
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-16 15:03:27 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,29 +7,22 @@ def rotate_2d_matrix(matrix):
     Rotates a 2D matrix 90 degrees clockwise.
     The rotation is done in place.
     """
-    n = len(matrix)  # Get the size of the matrix (n x n)
+    l, r = 0, len(matrix) - 1
 
-    # Loop through each layer (or ring) of the matrix from outermost to
-    # innermost
-    for layer in range(n // 2):
-        first = layer  # Index of the first element in the layer
-        last = n - 1 - layer  # Index of the last element in the layer
+    while l < r:
+        for i in range(r - l):
+            top, bottom = l, r
 
-        # Loop through the elements in the current layer
-        for i in range(first, last):
-            # Offset is used to move elements within the layer
-            offset = i - first
-            # Save the top element temporarily
-            top = matrix[first][i]
+            # Save the top-left value
+            topLeft = matrix[top][l + i]
+            # Move bottom-left to top-left
+            matrix[top][l + i] = matrix[bottom - i][l]
+            # Move bottom-right to bottom-left
+            matrix[bottom - i][l] = matrix[bottom][r - i]
+            # Move top-right to bottom-right
+            matrix[bottom][r - i] = matrix[top + i][r]
+            # Move saved top-left to top-right
+            matrix[top + i][r] = topLeft
 
-            # Move the left element to the top
-            matrix[first][i] = matrix[last - offset][first]
-
-            # Move the bottom element to the left
-            matrix[last - offset][first] = matrix[last][last - offset]
-
-            # Move the right element to the bottom
-            matrix[last][last - offset] = matrix[i][last]
-
-            # Move the saved top element to the right
-            matrix[i][last] = top
+        r -= 1
+        l += 1
